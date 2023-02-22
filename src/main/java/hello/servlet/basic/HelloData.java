4fafb2e65c9263cd2822e8df88e8f70da0865e9e
ssuu7861 <ssuu7861@gmail.com>
2023-02-23 06:46:10 +0900

    section 2. HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter // lombok -> getter setter 만들어줌
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
